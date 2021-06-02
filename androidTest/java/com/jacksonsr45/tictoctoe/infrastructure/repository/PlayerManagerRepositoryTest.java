fbe12102c31452118ad11f6673f942a1c058d639
jacksonsr45 <jacksonsr45@gmail.com>
2021-06-02 19:07:45 -0300

    Refactoring PlayerManagerRepositoryTest

--- androidTest/java/com/jacksonsr45/tictoctoe/infrastructure/repository/PlayerManagerRepositoryTest.java
+++ androidTest/java/com/jacksonsr45/tictoctoe/infrastructure/repository/PlayerManagerRepositoryTest.java
@@ -7,6 +7,8 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import com.jacksonsr45.tictoctoe.domain.entity.playermanager.PlayerEntity;
 import com.jacksonsr45.tictoctoe.domain.request.playermanager.PlayerRequest;
 import com.jacksonsr45.tictoctoe.domain.response.playermanager.PlayerResponse;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,16 +18,33 @@ import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
 public class PlayerManagerRepositoryTest {
-    private Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-    private PlayerManagerRepository playerManagerRepository = new PlayerManagerRepository(appContext);
+    private Context appContext;
+    private PlayerManagerRepository playerManagerRepository;
     private ArrayList<PlayerResponse> players;
     private PlayerResponse response;
-    private PlayerRequest request, request1, request2, request3;
-    private PlayerEntity entity, entity1, entity2, entity3;
+    private PlayerRequest request, request1, request2, request3, requestUp;
+    private PlayerEntity entity, entity1, entity2, entity3, entityUp;
     private String name = "Test One";
     private String name1 = "Test Two";
     private String name2 = "Test Three";
     private String name3 = "Test Four";
+
+    @Before
+    public void initContextAndDefaultsComponents() {
+        appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        playerManagerRepository = new PlayerManagerRepository(appContext);
+
+        this.request = new PlayerRequest(this.name);
+        this.entity = new PlayerEntity(request);
+        this.request1 = new PlayerRequest(this.name1);
+        this.entity1 = new PlayerEntity(request1);
+        this.request2 = new PlayerRequest(this.name2);
+        this.entity2 = new PlayerEntity(request2);
+        this.request3 = new PlayerRequest(this.name3);
+        this.entity3 = new PlayerEntity(request3);
+        this.requestUp = new PlayerRequest(request.getId(), this.name1);
+        this.entityUp = new PlayerEntity(this.requestUp);
+    }
 
     @Test
     public void shouldBeAddPlayerInsertInDBValues() {
@@ -37,13 +56,10 @@ public class PlayerManagerRepositoryTest {
 
         assertEquals(request.getId(), response.id);
         assertEquals(this.name, response.name);
-        this.playerManagerRepository.deletePlayer(request.getId());
     }
 
     @Test
-    public void shouldBeDeletePlayerInDataBase() {
-        this.request = new PlayerRequest(this.name);
-        this.entity = new PlayerEntity(request);
+    public void shouldBeDeletePlayerInDataBaseAndReturnResponseNull() {
         this.playerManagerRepository.addPlayer(this.entity);
         this.playerManagerRepository.deletePlayer(this.request.getId());
 
@@ -53,47 +69,30 @@ public class PlayerManagerRepositoryTest {
     }
 
     @Test
-    public void shouldBeShowPlayerGetIdDataAndReturnValue() {
-        this.request = new PlayerRequest(this.name);
-        this.entity = new PlayerEntity(request);
+    public void shouldBeShowPlayerReceivePlayerIdAndCompareReturnDatabase() {
         this.playerManagerRepository.addPlayer(this.entity);
 
         this.response = this.playerManagerRepository.showPlayer(this.request.getId());
 
         assertEquals(this.request.getId(), this.response.id);
         assertEquals(this.name, this.response.name);
-        this.playerManagerRepository.deletePlayer(request.getId());
     }
 
     @Test
-    public void shouldBeUpdatePlayerSetValuesByUpdatePlayer() {
-        this.request = new PlayerRequest(this.name);
-        this.entity = new PlayerEntity(request);
-
+    public void shouldBeUpdatePlayerSetValuesByPlayerIdAndReturnValuesDatabaseUpdated() {
         this.playerManagerRepository.addPlayer(this.entity);
         this.response = this.playerManagerRepository.showPlayer(request.getId());
-        this.request1 = new PlayerRequest(request.getId(), this.name1);
-        this.entity = new PlayerEntity(this.request1);
-        this.response = this.playerManagerRepository.updatedPlayer(this.entity);
+        this.response = this.playerManagerRepository.updatedPlayer(this.entityUp);
 
         assertEquals(request.getId(), response.id);
         assertEquals(this.name1, response.name);
-        this.playerManagerRepository.deletePlayer(request.getId());
     }
 
     @Test
-    public void shouldBeListPlayerGetValuesAndCompareAreIsArrayListWithPlayerResponse() {
-        this.request = new PlayerRequest(this.name);
-        this.entity = new PlayerEntity(request);
+    public void shouldBeListPlayerGetValuesAndCompareArrayListWithPlayerResponse() {
         this.playerManagerRepository.addPlayer(this.entity);
-        this.request1 = new PlayerRequest(this.name1);
-        this.entity1 = new PlayerEntity(request1);
         this.playerManagerRepository.addPlayer(this.entity1);
-        this.request2 = new PlayerRequest(this.name2);
-        this.entity2 = new PlayerEntity(request2);
         this.playerManagerRepository.addPlayer(this.entity2);
-        this.request3 = new PlayerRequest(this.name3);
-        this.entity3 = new PlayerEntity(request3);
         this.playerManagerRepository.addPlayer(this.entity3);
 
         this.players = this.playerManagerRepository.listPlayers();
@@ -104,6 +103,10 @@ public class PlayerManagerRepositoryTest {
             assertNotEquals(null, test.id);
             assertNotEquals(null, test.name);
         }
+    }
+
+    @After
+    public void dropTable() {
         this.playerManagerRepository.deletePlayer(request.getId());
         this.playerManagerRepository.deletePlayer(request1.getId());
         this.playerManagerRepository.deletePlayer(request2.getId());
