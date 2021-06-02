3a8ecae7f1312b82da2733d41b757583bd0fd1b3
jacksonsr45 <jacksonsr45@gmail.com>
2021-06-02 19:53:58 -0300

    Refactoring PlayerManagerTest

--- test/java/com/jacksonsr45/tictoctoe/domain/usecases/playermanager/PlayerManagerTest.java
+++ test/java/com/jacksonsr45/tictoctoe/domain/usecases/playermanager/PlayerManagerTest.java
@@ -1,71 +1,80 @@
 package com.jacksonsr45.tictoctoe.domain.usecases.playermanager;
 
 import com.jacksonsr45.tictoctoe.domain.request.playermanager.PlayerRequest;
+import com.jacksonsr45.tictoctoe.domain.response.playermanager.PlayerResponse;
 import com.jacksonsr45.tictoctoe.domain.usecases.playermanager.presenter.PlayerManagerPresenter;
 import com.jacksonsr45.tictoctoe.domain.usecases.playermanager.repository.PlayerManagerRepository;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 import static org.junit.Assert.*;
 
 public class PlayerManagerTest {
-    String idOne = "53d051ec-f1c3-4b52-9878-12954563e035";
-    String idTwo = "53d051ec-f1c3-4b52-9878-12954563e057";
-    String idThree = "53d051ec-f1c3-4b52-9878-12954563e084";
-    String nameOne = "Test One";
-    String nameTwo = "Test Two";
-    String nameThree = "Test Three";
+    PlayerManagerRepository repository;
+    PlayerManager playerManager;
+    PlayerRequest request;
+    PlayerManagerPresenter presenter, presenterOne, presenterTwo, presenterThree;
+    String idOne, idTwo, idThree, nameOne, nameTwo, nameThree;
+
+    @Before
+    public void initDefault() {
+        repository = new PlayerManagerRepository();
+        playerManager = new PlayerManager(repository);
+        presenter = new PlayerManagerPresenter();
+        presenterOne = new PlayerManagerPresenter();
+        presenterTwo = new PlayerManagerPresenter();
+        presenterThree = new PlayerManagerPresenter();
+
+        idOne = "53d051ec-f1c3-4b52-9878-12954563e035";
+        idTwo = "53d051ec-f1c3-4b52-9878-12954563e057";
+        idThree = "53d051ec-f1c3-4b52-9878-12954563e084";
+        nameOne = "Test One";
+        nameTwo = "Test Two";
+        nameThree = "Test Three";
+
+        request = new PlayerRequest(nameOne);
+    }
 
     @Test
-    public void shouldBeAddPlayerAndReturnValuePresent() {
-        PlayerManagerRepository repository = new PlayerManagerRepository();
-        PlayerManager playerManager = new PlayerManager(repository);
-        PlayerRequest request = new PlayerRequest(nameOne);
-        PlayerManagerPresenter presenter = new PlayerManagerPresenter();
-
+    public void shouldBeAddPlayerReturnPlayerManagerResponseAndCompare() {
         playerManager.addPlayer(request, presenter);
 
+        assertEquals(PlayerResponse.class, presenter.getPlayer().getClass());
         assertEquals(nameOne, presenter.getPlayer().name);
         assertNotEquals(null, presenter.getPlayer().id);
     }
 
     @Test
-    public void shouldBeUpdatePlayerAndReturnValuePresent() {
-        PlayerManagerRepository repository = new PlayerManagerRepository();
-        PlayerManager playerManager = new PlayerManager(repository);
-        PlayerRequest request = new PlayerRequest(nameOne);
-        PlayerManagerPresenter presenter = new PlayerManagerPresenter();
-
-        String id = UUID.randomUUID().toString();
+    public void shouldBeUpdatePlayerReturnPlayerManagerResponseAndCompare() {
         playerManager.addPlayer(request, presenter);
         request.setId(idTwo);
         request.setName(nameTwo);
         playerManager.updatePlayer(request, presenter);
 
+        assertEquals(PlayerResponse.class, presenter.getPlayer().getClass());
         assertEquals(nameTwo, presenter.getPlayer().name);
         assertEquals(idTwo, presenter.getPlayer().id);
     }
 
     @Test
-    public void shouldBeDeletePlayerReturnValue() {
-        PlayerManagerRepository repository = new PlayerManagerRepository();
-        PlayerManager playerManager = new PlayerManager(repository);
-        PlayerManagerPresenter presenter = new PlayerManagerPresenter();
-
+    public void shouldBeDeletePlayerReturnPlayerManagerResponseAndCompare() {
         playerManager.deletePlayer(idOne, presenter);
 
+        assertEquals(PlayerResponse.class, presenter.getPlayer().getClass());
         assertNull(null, presenter.getPlayer().id);
         assertNull(null, presenter.getPlayer().name);
     }
 
     @Test
-    public void shouldBeListPlayersReturnValue() {
-        PlayerManagerRepository repository = new PlayerManagerRepository();
-        PlayerManager playerManager = new PlayerManager(repository);
-        PlayerManagerPresenter presenter = new PlayerManagerPresenter();
+    public void shouldBeListPlayersReturnArrayListWithPlayerManagerResponseAndCompare() {
         playerManager.listPlayers(presenter);
 
+        assertEquals(ArrayList.class, presenter.getPlayers().getClass());
+        assertEquals(PlayerResponse.class, presenter.getPlayers().get(0).getClass());
         assertEquals(idOne, presenter.getPlayers().get(0).id);
         assertEquals(nameOne, presenter.getPlayers().get(0).name);
         assertEquals(idTwo, presenter.getPlayers().get(1).id);
@@ -75,22 +84,38 @@ public class PlayerManagerTest {
     }
 
     @Test
-    public void shouldBeShowPlayersReturnValue() {
-        PlayerManagerRepository repository = new PlayerManagerRepository();
-        PlayerManager playerManager = new PlayerManager(repository);
-        PlayerManagerPresenter presenterOne = new PlayerManagerPresenter();
-        PlayerManagerPresenter presenterTwo = new PlayerManagerPresenter();
-        PlayerManagerPresenter presenterThree = new PlayerManagerPresenter();
-
+    public void shouldBeShowPlayerReturnPlayerManagerResponseAndCompare() {
         playerManager.showPlayer( idOne, presenterOne);
         playerManager.showPlayer( idTwo, presenterTwo);
         playerManager.showPlayer( idThree, presenterThree);
 
+        assertEquals(PlayerResponse.class, presenterOne.getPlayer().getClass());
         assertEquals(idOne, presenterOne.getPlayer().id);
         assertEquals(nameOne, presenterOne.getPlayer().name);
+        assertEquals(PlayerResponse.class, presenterTwo.getPlayer().getClass());
         assertEquals(idTwo, presenterTwo.getPlayer().id);
         assertEquals(nameTwo, presenterTwo.getPlayer().name);
+        assertEquals(PlayerResponse.class, presenterThree.getPlayer().getClass());
         assertEquals(idThree, presenterThree.getPlayer().id);
         assertEquals(nameThree, presenterThree.getPlayer().name);
+    }
+
+    @After
+    public void setNull() {
+        repository = null;
+        playerManager = null;
+        presenter = null;
+        presenterOne = null;
+        presenterTwo = null;
+        presenterThree = null;
+
+        idOne = null;
+        idTwo = null;
+        idThree = null;
+        nameOne = null;
+        nameTwo = null;
+        nameThree = null;
+
+        request = null;
     }
 }
