ed977f440a29816dcf38039d0e805712a5bcf433
jacksonsr45 <jacksonsr45@gmail.com>
2021-06-02 19:31:35 -0300

    Refactoring PlayerHistoryManagerTest

--- test/java/com/jacksonsr45/tictoctoe/domain/usecases/playerhistorymanager/PlayerHistoryManagerTest.java
+++ test/java/com/jacksonsr45/tictoctoe/domain/usecases/playerhistorymanager/PlayerHistoryManagerTest.java
@@ -2,8 +2,12 @@ package com.jacksonsr45.tictoctoe.domain.usecases.playerhistorymanager;
 
 import com.jacksonsr45.tictoctoe.domain.request.playerhistory.PlayerHistoryRequest;
 import com.jacksonsr45.tictoctoe.domain.response.playerhistory.MatchResponse;
+import com.jacksonsr45.tictoctoe.domain.response.playerhistory.MovementsResponse;
+import com.jacksonsr45.tictoctoe.domain.response.playerhistory.PlayerHistoryResponse;
 import com.jacksonsr45.tictoctoe.domain.usecases.playerhistorymanager.presenter.PlayerHistoryPresenter;
 import com.jacksonsr45.tictoctoe.domain.usecases.playerhistorymanager.repository.PlayerHistoryRepository;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -12,36 +16,37 @@ import java.util.UUID;
 import static org.junit.Assert.*;
 
 public class PlayerHistoryManagerTest {
-    String matchId1 = "afa36c80-8172-4370-8914-d4a148fa3128";
-    String matchId2 = "afa36c80-8172-4370-8914-d4a148fa3987";
-    String matchId3 = "afa36c80-8172-4370-8914-d4a148fa3456";
-    String historyId1 = "afa36c80-8172-4370-8914-d4a148fa3128";
-    String historyId2 = "afa36c80-8172-4370-8914-d4a148fa3175";
-    String historyId3 = "afa36c80-8172-4370-8914-d4a148fa3116";
-    String playerId1 = "afa36c80-8172-4370-8914-d4a148fa3128";
+    private PlayerHistoryRepository repository;
+    private PlayerHistoryManager playerHistory;
+    private PlayerHistoryPresenter presenter;
+    private String matchId1, historyId1, historyId2, historyId3, playerId1;
 
-    String id = UUID.randomUUID().toString();
-    int victories = 2, defeats = 2, ties = 1, total = 5;
+    @Before
+    public void initComponentsDefaults() {
+        repository = new PlayerHistoryRepository();
+        playerHistory = new PlayerHistoryManager(repository);
+        presenter = new PlayerHistoryPresenter();
+
+        matchId1 = "afa36c80-8172-4370-8914-d4a148fa3128";
+        historyId1 = "afa36c80-8172-4370-8914-d4a148fa3128";
+        historyId2 = "afa36c80-8172-4370-8914-d4a148fa3175";
+        historyId3 = "afa36c80-8172-4370-8914-d4a148fa3116";
+        playerId1 = "afa36c80-8172-4370-8914-d4a148fa3128";
+    }
 
     @Test
-    public void shouldBeCreatePlayerHistoryWithValuesUserAndGetReturn() {
-        PlayerHistoryRepository repository = new PlayerHistoryRepository();
-        PlayerHistoryManager playerHistory = new PlayerHistoryManager(repository);
+    public void shouldBeCreatePlayerHistoryAndCompareResult() {
         PlayerHistoryRequest request = new PlayerHistoryRequest(playerId1);
-        PlayerHistoryPresenter presenter = new PlayerHistoryPresenter();
 
         playerHistory.createPlayerHistory(request, presenter);
 
+        assertEquals(PlayerHistoryResponse.class, presenter.getPlayerHistory().getClass());
         assertNotEquals(null, presenter.getPlayerHistory().id);
         assertEquals(playerId1, presenter.getPlayerHistory().playerId);
     }
 
     @Test
     public void shouldBeDeletePlayerHistoryByPlayerID() {
-        PlayerHistoryRepository repository = new PlayerHistoryRepository();
-        PlayerHistoryManager playerHistory = new PlayerHistoryManager(repository);
-        PlayerHistoryPresenter presenter = new PlayerHistoryPresenter();
-
         playerHistory.deletePlayerHistory(playerId1, presenter);
 
         assertEquals(null, presenter.getPlayerHistory());
@@ -49,12 +54,9 @@ public class PlayerHistoryManagerTest {
 
     @Test
     public void shouldBeShowPlayerHistoryReturnPlayerValue() {
-        PlayerHistoryRepository repository = new PlayerHistoryRepository();
-        PlayerHistoryManager playerHistory = new PlayerHistoryManager(repository);
-        PlayerHistoryPresenter presenter = new PlayerHistoryPresenter();
-
         playerHistory.showPlayerHistory(playerId1, presenter);
 
+        assertEquals(PlayerHistoryResponse.class, presenter.getPlayerHistory().getClass());
         assertEquals(historyId1, presenter.getPlayerHistory().id);
         assertEquals(playerId1, presenter.getPlayerHistory().playerId);
         assertEquals(1, presenter.getPlayerHistory().victories);
@@ -64,13 +66,11 @@ public class PlayerHistoryManagerTest {
     }
 
     @Test
-    public void shouldBeListMatchReturnListWithValues() {
-        PlayerHistoryRepository repository = new PlayerHistoryRepository();
-        PlayerHistoryManager playerHistory = new PlayerHistoryManager(repository);
-        PlayerHistoryPresenter presenter = new PlayerHistoryPresenter();
-
+    public void shouldBeListMatchReturnArrayListWithValues() {
         playerHistory.listMatch( historyId1, presenter);
 
+        assertEquals(ArrayList.class, presenter.getMatches().getClass());
+        assertEquals(MatchResponse.class, presenter.getMatches().get(0).getClass());
         assertEquals(1, presenter.getMatches().get(0).level);
         assertEquals(historyId1, presenter.getMatches().get(0).playersHistoryId);
         assertEquals(2, presenter.getMatches().get(1).level);
@@ -80,25 +80,20 @@ public class PlayerHistoryManagerTest {
     }
 
     @Test
-    public void shouldBeShouMatchReturnValuesById() {
-        PlayerHistoryRepository repository = new PlayerHistoryRepository();
-        PlayerHistoryManager playerHistory = new PlayerHistoryManager(repository);
-        PlayerHistoryPresenter presenter = new PlayerHistoryPresenter();
-
+    public void shouldBeShouMatchReturnValuesMatchResponseByMatchId() {
         playerHistory.showMatch(matchId1, presenter);
 
+        assertEquals(MatchResponse.class, presenter.getMatch().getClass());
         assertEquals(matchId1, presenter.getMatch().id);
         assertEquals(historyId1, presenter.getMatch().playersHistoryId);
     }
 
     @Test
-    public void shouldBeListMovementsReturnAllMovementsInMatch() {
-        PlayerHistoryRepository repository = new PlayerHistoryRepository();
-        PlayerHistoryManager playerHistory = new PlayerHistoryManager(repository);
-        PlayerHistoryPresenter presenter = new PlayerHistoryPresenter();
-
+    public void shouldBeListMovementsReturnAllMovementsInMatchInArrayListWithMovementsResponse() {
         playerHistory.listMovements(matchId1, presenter);
 
+        assertEquals(ArrayList.class, presenter.getMovements().getClass());
+        assertEquals(MovementsResponse.class, presenter.getMovements().get(0).getClass());
         assertEquals(1, presenter.getMovements().get(0).id);
         assertEquals(matchId1, presenter.getMovements().get(0).matchId);
         assertEquals(0, presenter.getMovements().get(0).line);
@@ -114,5 +109,17 @@ public class PlayerHistoryManagerTest {
         assertEquals(2, presenter.getMovements().get(2).line);
         assertEquals(2, presenter.getMovements().get(2).column);
         assertEquals(1, presenter.getMovements().get(2).value);
+    }
+
+    @After
+    public void setNull() {
+        repository = null;
+        playerHistory = null;
+        presenter = null;
+        matchId1 = null;
+        historyId1 = null;
+        historyId2 = null;
+        historyId3 = null;
+        playerId1 = null;
     }
 }
