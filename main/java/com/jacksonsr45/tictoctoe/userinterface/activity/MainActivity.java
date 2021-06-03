60a586b1ef02c7bc3a0232166890da42462d5cc0
jacksonsr45 <jacksonsr45@gmail.com>
2021-06-03 19:34:07 -0300

    set values defaults, and creating landingPageLayout

---
+++ main/java/com/jacksonsr45/tictoctoe/userinterface/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jacksonsr45.tictoctoe.userinterface.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.jacksonsr45.tictoctoe.userinterface.activit.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
+}
