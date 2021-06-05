5575aa886d3f880edc9e3e99b240b407d2415019
jacksonsr45 <jacksonsr45@gmail.com>
2021-06-05 16:54:00 -0300

    Add activity_game_result.xml

--- main/java/com/jacksonsr45/tictoctoe/userinterface/activity/MainActivity.java
+++ main/java/com/jacksonsr45/tictoctoe/userinterface/activity/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.testIntent = new Intent(getApplicationContext(), InGameActivity.class);
+        this.testIntent = new Intent(getApplicationContext(), GameResultActivity.class);
         startActivity(this.testIntent);
     }
 }
