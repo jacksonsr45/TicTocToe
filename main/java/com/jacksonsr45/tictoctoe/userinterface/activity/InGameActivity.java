80418ec10e13b431ea56a624034cf82abb8b269d
jacksonsr45 <jacksonsr45@gmail.com>
2021-06-05 16:04:03 -0300

    Add activity_in_game.xml

---
+++ main/java/com/jacksonsr45/tictoctoe/userinterface/activity/InGameActivity.java
@@ -0,0 +1,13 @@
+package com.jacksonsr45.tictoctoe.userinterface.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_game);
+    }
+}
--- main/java/com/jacksonsr45/tictoctoe/userinterface/activity/MainActivity.java
+++ main/java/com/jacksonsr45/tictoctoe/userinterface/activity/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.testIntent = new Intent(getApplicationContext(), ResultMatchActivity.class);
+        this.testIntent = new Intent(getApplicationContext(), InGameActivity.class);
         startActivity(this.testIntent);
     }
 }
