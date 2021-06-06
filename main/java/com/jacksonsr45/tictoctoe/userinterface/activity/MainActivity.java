7ef66feb9120ff8941776de8b76ae08634b4347c
jacksonsr45 <jacksonsr45@gmail.com>
2021-06-05 23:43:38 -0300

    Init MainActivity with check Players in db and set default page with result in landing page

--- main/java/com/jacksonsr45/tictoctoe/userinterface/activity/MainActivity.java
+++ main/java/com/jacksonsr45/tictoctoe/userinterface/activity/MainActivity.java
@@ -3,17 +3,32 @@ package com.jacksonsr45.tictoctoe.userinterface.activity;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.jacksonsr45.tictoctoe.domain.usecases.playermanager.PlayerManager;
+import com.jacksonsr45.tictoctoe.infrastructure.repository.PlayerManagerRepository;
 import com.jacksonsr45.tictoctoe.userinterface.activity.R;
+import com.jacksonsr45.tictoctoe.userinterface.presenter.PlayerManagerPresenter;
 
 public class MainActivity extends AppCompatActivity {
-    private Intent testIntent;
+    private Intent Intent;
+    private PlayerManagerRepository repository;
+    private PlayerManager player;
+    private PlayerManagerPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.repository = new PlayerManagerRepository(getApplicationContext());
+        this.player = new PlayerManager(this.repository);
+        this.presenter = new PlayerManagerPresenter();
+        this.checkStartDefaultPage();
+    }
 
-        this.testIntent = new Intent(getApplicationContext(), AboutActivity.class);
-        startActivity(this.testIntent);
+    public void checkStartDefaultPage() {
+        this.player.listPlayers(this.presenter);
+        if (this.presenter.getPlayers().size() > 0)
+            this.Intent = new Intent(getApplicationContext(), SelectPlayerActivity.class);
+        else this.Intent = new Intent(getApplicationContext(), AddPlayerActivity.class);
+        startActivity(this.Intent);
     }
 }
