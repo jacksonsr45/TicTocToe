392e0bfbb639e4f03503c577dda46e72abe6b763
jacksonsr45 <jacksonsr45@gmail.com>
2021-06-07 00:27:48 -0300

    set buttons level and default paths values end set by InGameActivity

--- main/java/com/jacksonsr45/tictoctoe/userinterface/activity/HomeActivity.java
+++ main/java/com/jacksonsr45/tictoctoe/userinterface/activity/HomeActivity.java
@@ -8,18 +8,16 @@ import com.jacksonsr45.tictoctoe.userinterface.activity.R;
 import com.jacksonsr45.tictoctoe.userinterface.fragments.PlayerHistoryFragment;
 import com.jacksonsr45.tictoctoe.userinterface.fragments.SelectLevelFragment;
 import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 public class HomeActivity extends AppCompatActivity {
-    private Bundle extras;
+    private Bundle extras, bundle;
     private SmartTabLayout tabLayout;
     private ViewPager viewPager;
 
     private String playerID;
-
-    private SelectLevelFragment selectLevelFragment;
-    private PlayerHistoryFragment playerHistoryFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,20 +25,19 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         this.extras = getIntent().getExtras();
         this.playerID = this.extras.getString("playerID");
-
-        this.selectLevelFragment = new SelectLevelFragment();
-        this.playerHistoryFragment = new PlayerHistoryFragment();
-        this.selectLevelFragment.newInstance(this.playerID);
-        this.playerHistoryFragment.newInstance(this.playerID);
+        this.bundle = new Bundle();
+        this.bundle.putString("playerID", this.playerID);
 
         this.tabLayout = findViewById(R.id.tabLayout);
         this.viewPager = findViewById(R.id.viewPager);
 
+        FragmentPagerItems pages = new FragmentPagerItems(getApplicationContext());
+        pages.add(FragmentPagerItem.of(
+                getString(R.string.text_select_level), SelectLevelFragment.class, this.bundle));
+        pages.add(FragmentPagerItem.of(
+                getString(R.string.text_player_history), PlayerHistoryFragment.class, this.bundle));
         FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
-              getSupportFragmentManager(), FragmentPagerItems.with(getApplicationContext())
-                .add(R.string.text_select_level, SelectLevelFragment.class)
-                .add(R.string.text_player_history, PlayerHistoryFragment.class)
-                .create()
+                getSupportFragmentManager(), pages
         );
 
         this.viewPager.setAdapter(adapter);
--- main/java/com/jacksonsr45/tictoctoe/userinterface/fragments/SelectLevelFragment.java
+++ main/java/com/jacksonsr45/tictoctoe/userinterface/fragments/SelectLevelFragment.java
@@ -1,17 +1,31 @@
 package com.jacksonsr45.tictoctoe.userinterface.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.jacksonsr45.tictoctoe.domain.request.playerhistory.PlayerHistoryRequest;
+import com.jacksonsr45.tictoctoe.domain.usecases.playerhistorymanager.PlayerHistoryManager;
+import com.jacksonsr45.tictoctoe.infrastructure.repository.PlayerHistoryRepository;
+import com.jacksonsr45.tictoctoe.userinterface.activity.InGameActivity;
 import com.jacksonsr45.tictoctoe.userinterface.activity.R;
+import com.jacksonsr45.tictoctoe.userinterface.presenter.PlayerHistoryPresenter;
 
 
 public class SelectLevelFragment extends Fragment {
+    private Intent intent;
+    private PlayerHistoryRequest request;
+    private PlayerHistoryPresenter presenter;
+    private PlayerHistoryRepository repository;
+    private PlayerHistoryManager playerHistoryManager;
+
     private static final String ARG_PLAYER_ID = "playerID";
 
-    private String playerID;
+    private String playerID, playerHistoryID;
+    private ImageButton buttonLevelEasy, buttonLevelMedium, buttonLevelHard, buttonExit;
 
     public SelectLevelFragment() { }
 
@@ -27,7 +41,7 @@ public class SelectLevelFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            playerID = getArguments().getString(ARG_PLAYER_ID);
+            this.playerID = getArguments().getString(ARG_PLAYER_ID);
         }
     }
 
@@ -35,7 +49,72 @@ public class SelectLevelFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_select_level, container, false);
-//        System.out.println(getArguments().getString(ARG_PLAYER_ID));
+        this.repository = new PlayerHistoryRepository(getContext());
+        this.playerHistoryManager = new PlayerHistoryManager(this.repository);
+        this.presenter = new PlayerHistoryPresenter();
+        this.checkPlayerHistoricExist();
+        this.buttonLevelEasy = view.findViewById(R.id.buttonLevelEasy);
+        this.buttonLevelMedium = view.findViewById(R.id.buttonLevelMedium);
+        this.buttonLevelHard = view.findViewById(R.id.buttonLevelHard);
+        this.buttonExit = view.findViewById(R.id.buttonExit);
+
+        this.handleLevelEasy();
+        this.handleLevelMedium();
+        this.handleLevelHard();
+        this.exit();
         return view;
+    }
+
+    public void checkPlayerHistoricExist() {
+        this.request = new PlayerHistoryRequest(this.playerID);
+        this.playerHistoryManager.showPlayerHistory(this.playerID, this.presenter);
+        if (this.presenter != null )
+            this.playerHistoryID = this.presenter.getPlayerHistory().id;
+        else this.playerHistoryManager.createPlayerHistory(this.request, this.presenter);
+    }
+
+    public void handleLevelEasy() {
+        this.buttonLevelEasy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(getContext(), InGameActivity.class);
+                intent.putExtra("level", 1);
+                intent.putExtra("playerHistoryID", playerHistoryID);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void handleLevelMedium() {
+        this.buttonLevelMedium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(getContext(), InGameActivity.class);
+                intent.putExtra("level", 2);
+                intent.putExtra("playerHistoryID", playerHistoryID);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void handleLevelHard() {
+        this.buttonLevelHard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(getContext(), InGameActivity.class);
+                intent.putExtra("level", 3);
+                intent.putExtra("playerHistoryID", playerHistoryID);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void exit() {
+        this.buttonExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finishAffinity();
+            }
+        });
     }
 }
